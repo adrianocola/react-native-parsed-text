aa704e5df17ef3f2775882c7a4de7acb5569cd79
Adriano Cola <adrianocola@gmail.com>
2019-01-28 01:20:36 -0200

    Nested parsing

--- src/ParsedText.js
+++ src/ParsedText.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import TextExtraction from './lib/TextExtraction';
 
 export const PATTERNS = {
-  url: /(https?:\/\/|www\.)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*[-a-zA-Z0-9@:%_\+~#?&\/=])*/i,
+  url: /(https?:\/\/|www\.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/i,
   phone: /[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,7}/,
   email: /\S+@\S+\.\S+/,
 };
@@ -55,33 +55,57 @@ class ParsedText extends React.Component {
     });
   }
 
+  generateComponent(tokens, token, tokenName) {
+    let count = 0;
+    let text = token.text;
+    const components = [];
+    let match;
+    do{
+      match = text.match(/\{\{(TOKEN\-[0-9]+\-[0-9]+)\}\}/);
+      if(match){
+        let textBefore = text.substr(0, match.index);
+        if(textBefore){
+          components.push(<ReactNative.Text {...this.props.childrenProps} {...token.props} key={count++}>{textBefore}</ReactNative.Text>);
+        }
+        const subTokenName = match[1];
+        const subToken = tokens[subTokenName];
+        components.push(this.generateComponent(tokens, subToken, subTokenName));
+        text = text.substr(match.index + match[0].length);
+      }else if(text){
+        components.push(<ReactNative.Text {...this.props.childrenProps} {...token.props} key={count++}>{text}</ReactNative.Text>);
+      }
+    }while(match);
+
+    return <ReactNative.Text {...this.props.childrenProps} {...token.props} key={tokenName}>{components}</ReactNative.Text>
+  }
+
   getParsedText() {
     if (!this.props.parse)                       { return this.props.children; }
     if (typeof this.props.children !== 'string') { return this.props.children; }
 
     const textExtraction = new TextExtraction(this.props.children, this.getPatterns());
 
-    return textExtraction.parse().map((props, index) => {
-      return (
-        <ReactNative.Text
-          key={`parsedText-${index}`}
-          {...this.props.childrenProps}
-          {...props}
-        />
-      );
-    });
+    const token = textExtraction.parse();
+    return (
+      <ReactNative.Text
+        {...this.props.childrenProps}
+      >
+        {this.generateComponent(token.tokens, token, 'token')}
+      </ReactNative.Text>
+    );
   }
 
   render() {
-    // Discard custom props before passing remainder to ReactNative.Text
-    const { parse, childrenProps, ...remainder } = { ...this.props };
-
     return (
-      <ReactNative.Text ref={ref => (this._root = ref)} {...remainder}>
+      <ReactNative.Text
+        ref={ref => this._root = ref}
+        {...this.props}
+      >
         {this.getParsedText()}
       </ReactNative.Text>
     );
   }
+
 }
 
 export default ParsedText;
